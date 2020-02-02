75f07327b02d473e7f234f00252675e27026ba7d
Noah Weinberg <30723865+noahweinberg@users.noreply.github.com>
2020-02-02 13:28:48 -0500

    Add files via upload

---
+++ introsent.py
@@ -0,0 +1,27 @@
+import tweepy as tp
+from textblob import TextBlob
+
+cons_key = 'yvTVlnvwHw2MPCWIo2TkYR75k'
+cons_secret = 'c3WUqknCWbiaxwpIELfhyfOEBfMM3iFEBm0sw3nro7TdeBkFmN'
+
+a_token = '1205694345325809664-6cpxodbVxPSwlFc6DVxwvrjutRS5qE'
+a_token_secret = 'sdhU3kLTeojBdEN4I9pr8LDQplYKqPgfQ69Jb41bPlDGw'
+
+auth = tp.OAuthHandler(cons_key, cons_secret)
+auth.set_access_token(a_token, a_token_secret)
+
+api = tp.API(auth)
+
+public_tweets = api.search('Coronavirus')
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	print(analysis.sentiment)
+
+if (analysis.sentiment.polarity < 0):
+	print("this is bad")
+	
+else:
+	print("this is good")
+	
